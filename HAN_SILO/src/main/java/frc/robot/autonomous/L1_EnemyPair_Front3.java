5984bc2ff39f4b89c80f29de97d9b68a2db832bd
daniel <rdanielbuckley@gmail.com>
2021-02-09 18:53:02 -0500

    ARCADE robot for TEST BENCH RIO

--- HAN_SILO/src/main/java/frc/robot/autonomous/L1_EnemyPair_Front3.java
+++ HAN_SILO/src/main/java/frc/robot/autonomous/L1_EnemyPair_Front3.java
@@ -24,22 +24,22 @@ public class L1_EnemyPair_Front3 extends _DelayableStrafingAutoMode {
             new ParallelCommandGroup(
                 createSwerveCommand(m_robotDrive, "BACKWARD TO ENEMY PAIR", TrajectoryDirection.REV, 
                                     TrajectoryHeading.UNROTATE, -31, new double[][]
-                {{0.0, 0.0, 20},
-                  {-1.49, -0.65},    // initial pose
-                  {-2.4, -0.75},    // initial pose
-                  {-2.3, -0.5},    // initial pose
-                 {-2.66, -0.24,-20}}      // final pose
+                {{0.0, 0.0, 20},      // initial pose
+                  {-1.49, -0.65},     // waypoint
+                  {-2.4, -0.75},      // waypoint
+                  {-2.3, -0.5},       // waypoint
+                 {-2.66, -0.24,-20}}  // final pose
                 ),
         
                 new IntakeSeqCommand(m_intake, m_sequence, true).withTimeout(5)
             ),
 
             new InstantCommand(m_shootclimb::enableShooting, m_shootclimb).withTimeout(4),
-            createSwerveCommand(m_robotDrive, "STRAFE TO SHOOT LOCATION 1", TrajectoryDirection.FWD, 
-                                TrajectoryHeading.UNROTATE, -15, new double[][]
-            {{-2.66, -0.24, -45},    // initial pose
-                {-0.18, -4.13, -45}} // final pose
-            ),
+                createSwerveCommand(m_robotDrive, "STRAFE TO SHOOT LOCATION 1", TrajectoryDirection.FWD, 
+                                    TrajectoryHeading.UNROTATE, -15, new double[][]
+                    {{-2.66, -0.24, -45}, // initial pose
+                     {-0.18, -4.13, -45}} // final pose
+                ),
 
             new WaitCommand(1),
             new ShootSeqCommandAuto(m_shootclimb, m_sequence).withTimeout(2),
@@ -50,8 +50,8 @@ public class L1_EnemyPair_Front3 extends _DelayableStrafingAutoMode {
                 createSwerveCommand(m_robotDrive, "BACKWARD TO FRONT 3", TrajectoryDirection.REV,
                                     TrajectoryHeading.MAINTAIN, 11, new double[][]
                 {{-0.18, -4.13, -10}, // initial pose
-                {-0.85, -3.8},      // waypoint(s)
-                 {-1.9, -4.03},      // waypoint(s)
+                {-0.85, -3.8},        // waypoint(s)
+                 {-1.9, -4.03},       // waypoint(s)
                  {-2.23, -3.27, 0}}   // final pose
                 ),
         
