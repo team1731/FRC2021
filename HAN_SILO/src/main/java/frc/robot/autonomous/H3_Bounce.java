2b98eebc280a30567809b109eb3d22d1e2b67f62
daniel <rdanielbuckley@gmail.com>
2021-02-09 21:36:19 -0500

    initial class definition -- copied from Brent

---
+++ HAN_SILO/src/main/java/frc/robot/autonomous/H3_Bounce.java
@@ -0,0 +1,47 @@
+package frc.robot.autonomous;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+import frc.robot.commands.IntakeSeqCommand;
+import frc.robot.commands.ShootSeqCommandAuto;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.JevoisVisionSubsystem;
+import frc.robot.subsystems.SequencerSubsystem;
+import frc.robot.subsystems.ShootClimbSubsystem;
+// This is the bounce path with the robot starting at the start one and driving forwards.
+public class H3_Bounce extends _DelayableStrafingAutoMode {
+    public H3_Bounce(DriveSubsystem m_robotDrive) {
+                
+        SequentialCommandGroup commandGroup = new SequentialCommandGroup(
+            new WaitCommand(getInitialDelaySeconds()),
+            createSwerveCommand(m_robotDrive, "Slalom ", TrajectoryDirection.REV, 
+            TrajectoryHeading.UNROTATE, 0, new double[][]
+                {{0.0, 0.0, 0},
+                {-0.762, -0.0},    
+                {-1.524, -0.762},    
+                {-2.286, -1.524},    
+                {-5.334, -1.524},    
+                {-5.842, -0.762}, 
+                {-6.858, -0.0},
+                {-7.62, -0.762},
+                {-6.858, -1.524},
+                {-5.842, -0.762},
+                {-5.334, 0.0},
+                {-2.286, 0.0},
+                {-1.524, -0.762},
+                {-0.762, -1.524},
+                {0.0, -1.524,0}}
+
+            ));
+
+        
+ 
+
+        // Run path following command, then stop at the end.
+        command = commandGroup.andThen(() -> m_robotDrive.drive(0, 0, 0, false));
+    }
+}
