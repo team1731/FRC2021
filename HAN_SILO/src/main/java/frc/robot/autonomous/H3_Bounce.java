3230777cac9515749515b9a6cd545461b41e7016
wade <wcullison@gmail.com>
2021-02-20 14:17:27 -0500

    mimic H1fig8

--- HAN_SILO/src/main/java/frc/robot/autonomous/H3_Bounce.java
+++ HAN_SILO/src/main/java/frc/robot/autonomous/H3_Bounce.java
@@ -12,45 +12,24 @@ public class H3_Bounce extends _DelayableStrafingAutoMode {
         SequentialCommandGroup commandGroup = new SequentialCommandGroup(
             new WaitCommand(getInitialDelaySeconds()),
             createSwerveCommand(m_robotDrive, "Bounce", TrajectoryDirection.REV, 
-            TrajectoryHeading.UNROTATE, 0, new double[][] 
-                {{0.0, 0.0, 0}, //NOTE: robot starts with its +x (longitudinal) axis aligned with field +x axis (facing the right side)
-                {-1, 0}, 
-                {-1, -0.5}, 
-                { -1, -1, 0}} //finish zone
-                ));   
-   /*
-                { 20,  -30},    
-                { 30,  -35},    
-                { 40,  -40}, 
-                { 50,  -45},
-                { 60,  -50}, //A3 pylon
+            TrajectoryHeading.UNROTATE, 0, new double[][]
+                {{0.0, 0.0, 0},
+                {-0.762, -0.0},    
+                {-1.524, -0.762},    
+                {-2.286, -1.524},    
+                {-5.334, -1.524},    
+                {-5.842, -0.762}, 
+                {-6.858, -0.0},
+                {-7.62, -0.762},
+                {-6.858, -1.524},
+                {-5.842, -0.762},
+                {-5.334, 0.0},
+                {-2.286, 0.0},
+                {-1.524, -0.762},
+                {-0.762, -1.524},
+                {0.0, -1.524,0}}
 
-                {-30,  -80},
-                {-60, -110}, //low point
-                {-30, -140},
-
-                { 30, -140},
-                { 60, -140}, //A6 pylon
-                { 30, -150},
-
-
-                {-30, -160},
-                {-55, -170}, //low points
-                {-55, -185},
-                {-55, -200}, //low points
-                {-30, -210},
-
-                { 30, -220},
-                { 60, -230}, //A9 pylon
-                { 30, -220},
-
-                { 30, -240}, 
-                { 10, -260},
-                {  0, -280, 0}} //finish zone 
-                {  0, -1, 0}} //finish zone
             ));
-
-*/
  
 
         // Run path following command, then stop at the end.
