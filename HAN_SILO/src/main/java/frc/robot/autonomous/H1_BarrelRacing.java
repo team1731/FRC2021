d4da357b9da944f9a9d60d754d6797aadda667bf
wade <wcullison@gmail.com>
2021-02-28 17:58:00 -0500

    Updated H1 & H2 Auto paths

--- HAN_SILO/src/main/java/frc/robot/autonomous/H1_BarrelRacing.java
+++ HAN_SILO/src/main/java/frc/robot/autonomous/H1_BarrelRacing.java
@@ -1,17 +1,9 @@
 package frc.robot.autonomous;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 
-import frc.robot.commands.IntakeSeqCommand;
-import frc.robot.commands.ShootSeqCommandAuto;
 import frc.robot.subsystems.DriveSubsystem;
-import frc.robot.subsystems.IntakeSubsystem;
-import frc.robot.subsystems.JevoisVisionSubsystem;
-import frc.robot.subsystems.SequencerSubsystem;
-import frc.robot.subsystems.ShootClimbSubsystem;
 
 public class H1_BarrelRacing extends _DelayableStrafingAutoMode {
     public H1_BarrelRacing(DriveSubsystem m_robotDrive) {
@@ -19,41 +11,44 @@ public class H1_BarrelRacing extends _DelayableStrafingAutoMode {
         SequentialCommandGroup commandGroup = new SequentialCommandGroup(
             new WaitCommand(getInitialDelaySeconds()),
 
-            // ENEMY PAIR
-            createSwerveCommand(m_robotDrive, "BARREL RACING", TrajectoryDirection.REV, 
-                                TrajectoryHeading.CONVERT_TO_METERS, 0, new double[][]
-                {{0.0, 0.0, -90},
-                {-41.8,  -9.6},    // 1
-                {-35.4,  -3.2},    // 2
-                {-38.6,   9.6},    // 3
-                {-16.1,  32.1},    // 4
-                { 22.5,  16.1},    // 5
-                {  9.6, -28.9},    // 6
-                {-22.5, -19.3},    // 7
-                {-19.3,   0.0},    // 8
-                {-32.1,  -3.2},    // 9
-                {-28.9,  -6.4},    // 10
-                {-32.1, -16.1},    // 11
-                { -6.4, -22.5},    // 12
-                { 28.9,  -6.4},    // 13
-                { 16.1,  16.1},    // 14
-                { -3.2,  22.5},    // 15
-                {-19.3,  32.1},    // 16
-                {-35.4,  22.5},    // 17
-                {-35.4,   6.4},    // 18
-                {-19.3, -28.9},    // 19
-                {-22.5, -22.5},    // 20
-                { 41.8,   0.0},    // 21
-                {  3.2,  -6.4},    // 22
-                { 32.1,  -6.4},    // 23
-                { 38.6,  -3.2},    // 24
-                { 41.8,   0.0},    // 25
-                { 35.4,   0.0},    // 26
-                {54.6, 0.0, 0}}    // 27 - final pose
-            )
-        );
+             createSwerveCommand(m_robotDrive, "BarrelRacing: start zone to around 1st circle", TrajectoryDirection.REV, 
+                 TrajectoryHeading.CONVERT_TO_METERS, 0, new double[][] { //these are INCHES
+                 {   0,    0, -90},
+                 {   0,  -30},
+                 {   0, -120},    
+                 { -30, -150},    
+                 { -60, -120},    
+                 { -30,  -90}, 
+                 {   0, -120,  0}   // 1st circle
+             }),
 
-        // Run path following command, then stop at the end.
+             createSwerveCommand(m_robotDrive, "BarrelRacing: around 2nd circle", TrajectoryDirection.REV, 
+                 TrajectoryHeading.CONVERT_TO_METERS, 0, new double[][] { //these are INCHES
+                 {   0, -120,  0},  // 1st circle
+                 {  15, -240}, 
+                 {  45, -240}, 
+                 {  45, -180}, 
+                 {  15, -180, 0}   // 2nd circle
+             }),
+        
+             createSwerveCommand(m_robotDrive, "BarrelRacing: around 3rd circle", TrajectoryDirection.REV, 
+                 TrajectoryHeading.CONVERT_TO_METERS, 0, new double[][] { //these are INCHES
+                 {  15, -180,  0},  // 2nd circle
+                 { -60, -240}, 
+                 { -60, -300}, 
+                 { -10, -300, 0}   // 3rd circle
+             }),
+                
+             createSwerveCommand(m_robotDrive, "BarrelRacing: back to finish", TrajectoryDirection.REV, 
+                 TrajectoryHeading.CONVERT_TO_METERS, 0, new double[][] { //these are INCHES
+                 { -10, -300, 0}, // 3rd circle
+                 {  -5, -180},
+                 {   0,    0, 0}  //finish zone
+             })
+         );
+
+         // Run path following command, then stop at the end.
         command = commandGroup.andThen(() -> m_robotDrive.drive(0, 0, 0, false));
     }
 }
+
