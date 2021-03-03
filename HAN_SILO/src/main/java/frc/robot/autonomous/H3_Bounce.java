506c75612e54a6339fe37ee0c73f9ee1b3d73418
Wade <wcullison@gmail.com>
2021-03-02 19:54:21 -0500

    Bounce auto

--- HAN_SILO/src/main/java/frc/robot/autonomous/H3_Bounce.java
+++ HAN_SILO/src/main/java/frc/robot/autonomous/H3_Bounce.java
@@ -12,54 +12,34 @@ public class H3_Bounce extends _DelayableStrafingAutoMode {
         SequentialCommandGroup commandGroup = new SequentialCommandGroup(
             new WaitCommand(getInitialDelaySeconds()),
 
-             createSwerveCommand(m_robotDrive, "Bounce: start zone to A3", TrajectoryDirection.REV, 
+             createSwerveCommand(m_robotDrive, "Bounce: start zone to A3", TrajectoryDirection.FWD, 
                  TrajectoryHeading.CONVERT_TO_METERS, 0, new double[][] { //these are INCHES
                  {0.0, 0.0, -90}, //NOTE: robot starts with its +x (longitudinal) axis aligned with field +x axis (facing the right side)
-                 {  0,  -10},    
-                 { 10,  -20},    
-                 { 20,  -30},    
-                 { 30,  -35},    
-                 { 40,  -40}, 
-                 { 50,  -45},
+                 {  0,  -50}, 
                  { 60,  -50, 0} //A3 pylon
              }),
 
              createSwerveCommand(m_robotDrive, "Bounce: A3 to A6", TrajectoryDirection.REV, 
                  TrajectoryHeading.CONVERT_TO_METERS, 0, new double[][] { //these are INCHES
                  {60,  -50, 0}, //A3 pylon
-
-                 {-30,  -80},
-                 {-60, -110}, //low point
+                 {-30, -50}, //low point
                  {-30, -140},
-
-                 { 30, -140},
                  { 60, -140, 0} //A6 pylon
              }),
         
              createSwerveCommand(m_robotDrive, "Bounce: A6 to A9", TrajectoryDirection.REV, 
                  TrajectoryHeading.CONVERT_TO_METERS, 0, new double[][] { //these are INCHES
                  { 60, -140, 0}, //A6 pylon
-                 { 30, -150},
-
-
-                 {-30, -160},
-                 {-55, -170}, //low points
-                 {-55, -185},
-                 {-55, -200}, //low points
-                 {-30, -210},
-
-                 { 30, -220},
+                 { -30, -140},
+                 { -30, -230},
                  { 60, -230, 0} //A9 pylon
              }),
         
              createSwerveCommand(m_robotDrive, "Bounce: A9 to finish zone", TrajectoryDirection.REV, 
                  TrajectoryHeading.CONVERT_TO_METERS, 0, new double[][] { //these are INCHES
                  { 60, -230, 0}, //A9 pylon
-                 { 30, -220},
-
-                 { 30, -240}, 
-                 { 10, -260}, 
-                 {  0, -280, 0}  //finish zone
+                 { 0, -230},
+                 {  0, -280, -180}  //finish zone
              })
          );
 /*
