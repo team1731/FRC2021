5e2cc4ea3ef9b448fb0cbffe157b27b87c680365
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2021-03-13 17:07:08 -0500

    Now looks for a PathWeaver path

--- HAN_SILO/src/main/java/frc/robot/autonomous/H0_GalacticSearch.java
+++ HAN_SILO/src/main/java/frc/robot/autonomous/H0_GalacticSearch.java
@@ -1,7 +1,19 @@
 package frc.robot.autonomous;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryUtil;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.IntakeExtend;
+import frc.robot.commands.IntakeRetract;
 import frc.robot.commands.IntakeSeqCommand;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
@@ -10,74 +22,94 @@ import frc.robot.subsystems.SequencerSubsystem;
 
 // This is the bounce path with the robot starting at the start one and driving forwards.
 public class H0_GalacticSearch extends _DelayableStrafingAutoMode {
+    Pose2d initialPose;
+
+    @Override
+    public Pose2d getInitialPose(){
+        return initialPose;
+    }
+
     public H0_GalacticSearch(DriveSubsystem m_robotDrive, IntakeSubsystem m_intake, SequencerSubsystem m_sequence,
     LimeLightSubsystem m_limelight) {
         SequentialCommandGroup commandGroup = null;
+        Pose2d initialPoseTrajectory;
         int field_orientation = m_limelight.getFieldOrientation(); 
         switch(field_orientation){
             case 0: //Red A (C3, D5, A6)
                 SmartDashboard.putString("SelectedGalactic", "RedA");
-                commandGroup = new SequentialCommandGroup(
-                    new IntakeSeqCommand(m_intake, m_sequence, true),
-                    createSwerveCommand(m_robotDrive, "Red A", TrajectoryDirection.REV, 
-                        TrajectoryHeading.CONVERT_TO_METERS, 0, new double[][] { //these are INCHES
-                        {0.0, 0.0, 0}, //NOTE: robot starts with its +x (longitudinal) axis aligned with field -x axis (facing the start zone)
-                        { -45, 0}, //Ball C3
-                        { -93, 30},   
-                        { -105, 30},//D5    
-                        { -123, -60},    
-                        { -135, -60}, // A6   
-                        { -300,  -60, 0} //Endzone
-                    })
-                );
+
+                Trajectory redATrajectory = new Trajectory();
+                try {
+                    Path redATrajectoryPath = Filesystem.getDeployDirectory().toPath().resolve("paths/output/RedPathA.wpilib.json");
+                    redATrajectory = TrajectoryUtil.fromPathweaverJson(redATrajectoryPath);
+                } catch (IOException ex){
+                    DriverStation.reportError("Unable to open RedA trajectory", ex.getStackTrace());
+                }
+
+                initialPoseTrajectory = redATrajectory.getInitialPose();
+                initialPose = new Pose2d(initialPoseTrajectory.getX(), initialPoseTrajectory.getY(), Rotation2d.fromDegrees(90));
+
+                commandGroup = new SequentialCommandGroup(new WaitCommand(getInitialDelaySeconds()),
+                new IntakeExtend(m_intake),
+                createSwerveCommand(m_robotDrive, "RedA: entire path", 90, redATrajectory),
+                new IntakeRetract(m_intake));
                 break;
             case 1: //Red B (B3, D5, B7)
                 SmartDashboard.putString("SelectedGalactic", "RedB");
-                commandGroup = new SequentialCommandGroup(
-                    new IntakeSeqCommand(m_intake, m_sequence, true),
-                    createSwerveCommand(m_robotDrive, "Red B", TrajectoryDirection.REV, 
-                        TrajectoryHeading.CONVERT_TO_METERS, 0, new double[][] { //these are INCHES
-                        {0.0, 0.0, 0}, //NOTE: robot starts with its +x (longitudinal) axis aligned with field -x axis (facing the start zone)
-                        { -45, 0}, //Ball B3
-                        { -93, 60},   
-                        { -105, 60},//D5    
-                        { -153, 0},    
-                        { -165, 0}, // B7   
-                        { -300, 0, 0} //Endzone
-                    })
-                );
+                
+                Trajectory redBTrajectory = new Trajectory();
+                try {
+                    Path redBTrajectoryPath = Filesystem.getDeployDirectory().toPath().resolve("paths/output/RedPathB.wpilib.json");
+                    redBTrajectory = TrajectoryUtil.fromPathweaverJson(redBTrajectoryPath);
+                } catch (IOException ex){
+                    DriverStation.reportError("Unable to open RedA trajectory", ex.getStackTrace());
+                }
+
+                initialPoseTrajectory = redBTrajectory.getInitialPose();
+                initialPose = new Pose2d(initialPoseTrajectory.getX(), initialPoseTrajectory.getY(), Rotation2d.fromDegrees(90));
+
+                commandGroup = new SequentialCommandGroup(new WaitCommand(getInitialDelaySeconds()),
+                new IntakeExtend(m_intake),
+                createSwerveCommand(m_robotDrive, "RedB: entire path", 90, redBTrajectory),
+                new IntakeRetract(m_intake));
                 break;
             case 2: //Blue A (E6, B7, C9)
                 SmartDashboard.putString("SelectedGalactic", "BlueA");
-                commandGroup = new SequentialCommandGroup(
-                    new IntakeSeqCommand(m_intake, m_sequence, true),
-                    createSwerveCommand(m_robotDrive, "Blue A", TrajectoryDirection.REV, 
-                        TrajectoryHeading.CONVERT_TO_METERS, 0, new double[][] { //these are INCHES
-                        {0.0, 0.0, 0}, //NOTE: robot starts with its +x (longitudinal) axis aligned with field -x axis (facing the start zone)
-                        { -135, 0}, //Ball E6
-                        { -153, -90},   
-                        { -165, -90},//B7    
-                        { -203, -60},    
-                        { -215, -60}, // C9   
-                        { -300,  -60, 0} //Endzone
-                    })
-                );
+                
+                Trajectory blueATrajectory = new Trajectory();
+                try {
+                    Path blueATrajectoryPath = Filesystem.getDeployDirectory().toPath().resolve("paths/output/BluePathA.wpilib.json");
+                    blueATrajectory = TrajectoryUtil.fromPathweaverJson(blueATrajectoryPath);
+                } catch (IOException ex){
+                    DriverStation.reportError("Unable to open RedA trajectory", ex.getStackTrace());
+                }
+
+                initialPoseTrajectory = blueATrajectory.getInitialPose();
+                initialPose = new Pose2d(initialPoseTrajectory.getX(), initialPoseTrajectory.getY(), Rotation2d.fromDegrees(90));
+
+                commandGroup = new SequentialCommandGroup(new WaitCommand(getInitialDelaySeconds()),
+                new IntakeExtend(m_intake),
+                createSwerveCommand(m_robotDrive, "BlueA: entire path", 90, blueATrajectory),
+                new IntakeRetract(m_intake));
                 break;
             case 3: //Blue B (D6, B8, D10)
                 SmartDashboard.putString("SelectedGalactic", "BlueB");
-                commandGroup = new SequentialCommandGroup(
-                    new IntakeSeqCommand(m_intake, m_sequence, true),
-                    createSwerveCommand(m_robotDrive, "Blue B", TrajectoryDirection.REV, 
-                        TrajectoryHeading.CONVERT_TO_METERS, 0, new double[][] { //these are INCHES
-                        {0.0, 0.0, 0}, //NOTE: robot starts with its +x (longitudinal) axis aligned with field -x axis (facing the start zone)
-                        { -135, 0}, //Ball D6
-                        { -183, -60},   
-                        { -195, -60},//B8    
-                        { -243, 0},    
-                        { -255, 0}, //D10   
-                        { -300, 0, 0} //Endzone
-                    })
-                );
+                
+                Trajectory blueBTrajectory = new Trajectory();
+                try {
+                    Path blueBTrajectoryPath = Filesystem.getDeployDirectory().toPath().resolve("paths/output/BluePathB.wpilib.json");
+                    blueBTrajectory = TrajectoryUtil.fromPathweaverJson(blueBTrajectoryPath);
+                } catch (IOException ex){
+                    DriverStation.reportError("Unable to open RedA trajectory", ex.getStackTrace());
+                }
+
+                initialPoseTrajectory = blueBTrajectory.getInitialPose();
+                initialPose = new Pose2d(initialPoseTrajectory.getX(), initialPoseTrajectory.getY(), Rotation2d.fromDegrees(90));
+
+                commandGroup = new SequentialCommandGroup(new WaitCommand(getInitialDelaySeconds()),
+                new IntakeExtend(m_intake),
+                createSwerveCommand(m_robotDrive, "BlueB: entire path", 90, blueBTrajectory),
+                new IntakeRetract(m_intake));
                 break;
         }       
         // Run path following command, then stop at the end.
