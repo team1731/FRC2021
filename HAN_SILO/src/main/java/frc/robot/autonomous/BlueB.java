f3b74873fd2355d725e8cf5b5c61e19e3b9a576b
Wade <wcullison@gmail.com>
2021-03-20 17:57:54 -0400

    color updates

--- HAN_SILO/src/main/java/frc/robot/autonomous/BlueB.java
+++ HAN_SILO/src/main/java/frc/robot/autonomous/BlueB.java
@@ -7,9 +7,9 @@ public class BlueB extends GalacticConfiguration {
     @Override
     public Translation2d[] getBallPositions(){
         return new Translation2d[] {
-            new Translation2d(0.11250, -0.22500),
-            new Translation2d(-0.35938, -0.125),
-            new Translation2d(0.08750, -0.07917)
+            new Translation2d(0.956, -0.00833),
+            new Translation2d(-0.2125, 0.0874999),
+            new Translation2d(0.10625, 0.1916666)
         };
     }
 
