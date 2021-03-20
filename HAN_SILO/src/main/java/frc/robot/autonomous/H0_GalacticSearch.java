9a1fa767424df9c045e7446c16564fdb8e3fb8b6
Wade <wcullison@gmail.com>
2021-03-20 17:03:39 -0400

    Galactic Search Red A almost working.

--- HAN_SILO/src/main/java/frc/robot/autonomous/H0_GalacticSearch.java
+++ HAN_SILO/src/main/java/frc/robot/autonomous/H0_GalacticSearch.java
@@ -16,10 +16,12 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.IntakeExtend;
 import frc.robot.commands.IntakeRetract;
 import frc.robot.commands.IntakeSeqCommand;
+import frc.robot.commands.ShootSeqCommandAuto;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.LimeLightSubsystem;
 import frc.robot.subsystems.SequencerSubsystem;
+import frc.robot.subsystems.ShootClimbSubsystem;
 
 // This is the bounce path with the robot starting at the start one and driving forwards.
 public class H0_GalacticSearch extends _DelayableStrafingAutoMode {
@@ -37,7 +39,7 @@ public class H0_GalacticSearch extends _DelayableStrafingAutoMode {
     }
 
     public H0_GalacticSearch(DriveSubsystem m_robotDrive, IntakeSubsystem m_intake, SequencerSubsystem m_sequence,
-    LimeLightSubsystem m_limelight) {
+    LimeLightSubsystem m_limelight, ShootClimbSubsystem m_shootclimb) {
         SequentialCommandGroup commandGroup = null;
         Pose2d initialPoseTrajectory;
         field_orientation = m_limelight.getFieldOrientation(); 
@@ -57,11 +59,12 @@ public class H0_GalacticSearch extends _DelayableStrafingAutoMode {
 
                     m_robotDrive.resetOdometry(initialPose); //because PathWeaver path uses absolute field coords
                     commandGroup = new SequentialCommandGroup(new WaitCommand(getInitialDelaySeconds()),
-                    new ParallelCommandGroup(
-                        new IntakeSeqCommand(m_intake, m_sequence, true),
-                        createSwerveCommand(m_robotDrive, "RedA: entire path", 180.0, redATrajectory)
-                    ),
-                    new IntakeRetract(m_intake));
+                        new ParallelCommandGroup(
+                            new IntakeSeqCommand(m_intake, m_sequence, true).withTimeout(6),
+                            createSwerveCommand(m_robotDrive, "RedA: entire path", 180.0, redATrajectory)
+                        ),
+                        new IntakeRetract(m_intake)
+                    );
                 } catch (IOException ex){
                     //System.out.println("Path not found: "+redATrajectoryPath);
                     DriverStation.reportError("Unable to open RedA trajectory", ex.getStackTrace());
@@ -84,11 +87,12 @@ public class H0_GalacticSearch extends _DelayableStrafingAutoMode {
                     initialPose = new Pose2d(initialPoseTrajectory.getX(), initialPoseTrajectory.getY(), Rotation2d.fromDegrees(180));
                     m_robotDrive.resetOdometry(initialPose); //because PathWeaver path uses absolute field coords
                     commandGroup = new SequentialCommandGroup(new WaitCommand(getInitialDelaySeconds()),
-                    new ParallelCommandGroup(
-                        new IntakeSeqCommand(m_intake, m_sequence, true),
-                        createSwerveCommand(m_robotDrive, "RedB: entire path", 180, redBTrajectory)
-                    ),
-                    new IntakeRetract(m_intake));
+                        new ParallelCommandGroup(
+                            new IntakeSeqCommand(m_intake, m_sequence, true).withTimeout(6),
+                            createSwerveCommand(m_robotDrive, "RedB: entire path", 180, redBTrajectory)
+                        ),
+                        new IntakeRetract(m_intake)
+                    );
                 } catch (IOException ex){
                     System.out.println("Path not found: "+redBTrajectoryPath);
                     DriverStation.reportError("Unable to open RedB trajectory", ex.getStackTrace());
@@ -109,11 +113,12 @@ public class H0_GalacticSearch extends _DelayableStrafingAutoMode {
                     initialPose = new Pose2d(initialPoseTrajectory.getX(), initialPoseTrajectory.getY(), Rotation2d.fromDegrees(180));
                     m_robotDrive.resetOdometry(initialPose); //because PathWeaver path uses absolute field coords
                     commandGroup = new SequentialCommandGroup(new WaitCommand(getInitialDelaySeconds()),
-                    new ParallelCommandGroup(
-                        new IntakeSeqCommand(m_intake, m_sequence, true),
-                        createSwerveCommand(m_robotDrive, "BlueA: entire path", 180, blueATrajectory)
-                    ),
-                    new IntakeRetract(m_intake));
+                        new ParallelCommandGroup(
+                            new IntakeSeqCommand(m_intake, m_sequence, true).withTimeout(6),
+                            createSwerveCommand(m_robotDrive, "BlueA: entire path", 180, blueATrajectory)
+                        ),
+                        new IntakeRetract(m_intake)     
+                    );
                 } catch (IOException ex){
                     System.out.println("Path not found: "+blueATrajectoryPath);
                     DriverStation.reportError("Unable to open BlueA trajectory", ex.getStackTrace());
@@ -133,12 +138,14 @@ public class H0_GalacticSearch extends _DelayableStrafingAutoMode {
                     initialPoseTrajectory = blueBTrajectory.getInitialPose();
                     initialPose = new Pose2d(initialPoseTrajectory.getX(), initialPoseTrajectory.getY(), Rotation2d.fromDegrees(180));
                     m_robotDrive.resetOdometry(initialPose); //because PathWeaver path uses absolute field coords
-                    commandGroup = new SequentialCommandGroup(new WaitCommand(getInitialDelaySeconds()),
-                    new ParallelCommandGroup(
-                        new IntakeSeqCommand(m_intake, m_sequence, true),
-                        createSwerveCommand(m_robotDrive, "BlueB: entire path", 180, blueBTrajectory)
-                    ),
-                    new IntakeRetract(m_intake));
+                    commandGroup = new SequentialCommandGroup(
+                        new WaitCommand(getInitialDelaySeconds()),
+                        new ParallelCommandGroup(
+                            new IntakeSeqCommand(m_intake, m_sequence, true).withTimeout(6),
+                            createSwerveCommand(m_robotDrive, "BlueB: entire path", 180, blueBTrajectory)
+                        ),
+                        new IntakeRetract(m_intake)
+                    );
                 } catch (IOException ex){
                     System.out.println("Path not found: "+blueBTrajectoryPath);
                     DriverStation.reportError("Unable to open BlueB trajectory", ex.getStackTrace());
@@ -147,7 +154,7 @@ public class H0_GalacticSearch extends _DelayableStrafingAutoMode {
                 break;
         }       
         // Run path following command, then stop at the end.
-        command = commandGroup.andThen(() -> m_robotDrive.drive(0, 0, 0, false));
+        command = commandGroup.andThen(() -> m_robotDrive.drive(0, 0, 0, false)).andThen(new ShootSeqCommandAuto(m_shootclimb, m_sequence).withTimeout(2)).andThen(() -> m_shootclimb.stopShooting());
     }
 }  
 
